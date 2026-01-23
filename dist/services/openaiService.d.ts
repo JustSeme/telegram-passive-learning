ad5e7bcbf9deb6b67643cd8808a6883460e0a5a9
JustSemen <semyn03@mail.ru>
2026-01-23 15:07:05 +0300

    fix: ignore dist

--- dist/services/openaiService.d.ts
+++
@@ -1,5 +0,0 @@
-import { QuestionData } from '../types';
-declare function generateQuestion(field: string, difficulty?: string): Promise<QuestionData>;
-declare function validateQuestion(questionData: QuestionData): Promise<boolean>;
-export { generateQuestion, validateQuestion };
-//# sourceMappingURL=openaiService.d.ts.map
--- dist/utils/logger.d.ts
+++
@@ -1,7 +0,0 @@
-import winston from 'winston';
-declare const logger: winston.Logger;
-declare const stream: {
-    write: (message: string) => void;
-};
-export { logger, stream };
-//# sourceMappingURL=logger.d.ts.map
